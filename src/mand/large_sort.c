8d3b988cac3672e2e874494fe75eaf516569dc70
gonzimaster <oscarpunticoma@gmail.com>
2022-08-19 20:02:36 +0200

    Change method of sorting chunks

--- src/mand/large_sort.c
+++ src/mand/large_sort.c
@@ -6,12 +6,14 @@
 /*   By: ogonzale <ogonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 12:16:51 by ogonzale          #+#    #+#             */
-/*   Updated: 2022/08/19 12:27:00 by ogonzale         ###   ########.fr       */
+/*   Updated: 2022/08/19 20:01:42 by ogonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "utils.h"
+#include "libft.h"
+#include <stdio.h>
 
 void	ft_split_by_sign(t_stck **stck_a, t_stck **stck_b, int size)
 {
@@ -105,39 +107,45 @@ void	ft_sort_chunks(t_stck **stck_a, t_stck **stck_b, int size)
 
 void	ft_sort_all(t_stck **stck_a, t_stck **stck_b, int size)
 {
-	int chunk_size;
 	int	pass;
 	int	size_a;
-	int	i;
+	int	size_b;
 	int	j;
+	t_stck	max;
 
+	printf("ft_sort_all\n");
 	pass = 0;
-	chunk_size = 5;
 	j = 0;
-	while (j < 4)
+	ft_get_max(&max, stck_a);
+	printf("max value = %d\n", max.value);
+	while (pass < 1)
 	{
-		while ((*stck_b)[0].size != chunk_size * (pass + 1))
+		size_a = (*stck_a)[0].size;
+		if ((*stck_b)[0].size == 0 && (*stck_a)[size_a - 1].value < (*stck_a)[size_a - 2].value
+				&& (*stck_a)[size - 1].value != max.value)
 			ft_push('b', stck_a, stck_b);
-		i = 0;
-		while (i < chunk_size * 2 * (pass + 1))
+		size_a = (*stck_a)[0].size;
+		size_b = (*stck_b)[0].size;
+		while ((*stck_a)[size_a - 1].value > (*stck_b)[size_b - 1].value
+				&& (*stck_a)[size - 1].value != max.value)
+		{
+			ft_push('b', stck_a, stck_b);
+			size_a = (*stck_a)[0].size;
+			size_b = (*stck_b)[0].size;
+		}
+		while ((*stck_b)[0].size != 0 && j < size)
 		{
 			size_a = (*stck_a)[0].size;
-			if (i == (chunk_size * 2 * (pass + 1) - 1) && (*stck_b)[0].size != 0)
-				ft_push('a', stck_a, stck_b);
-			else if ((*stck_a)[size_a - 1].value > (*stck_b)[0].value)
+			if ((*stck_a)[size_a - 1].value > (*stck_b)[0].value)
 			{
 				ft_rev_rotate('b', stck_b);
 				ft_push('a', stck_a, stck_b);
 			}
 			ft_rotate('a', stck_a);
-			i++;
-		}
-		//j = j + chunk_size * (pass + 1);
-		//if (j == size)
-		//{
 			j++;
-		//	j = 0;
-		//}
+		}
+		if (j == size)
+			pass++;
 	}
 	(void)size;
 }
